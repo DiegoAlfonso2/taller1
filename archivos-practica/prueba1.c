1b353c14193099ec90d2fad8aee1250d8c73c6ee
Diego <diego.alfonso2@gmail.com>
2016-07-09 16:27:01 -0300

    Lectura del contenido del archivo (en formato binario) con fgetc

--- archivos-practica/prueba1.c
+++ archivos-practica/prueba1.c
@@ -4,7 +4,11 @@ int main(int argc, char const *argv[])
 {
 	FILE* entrada;
 	fprintf(stdout, "Abriendo archivo\n");
-	entrada = fopen("prueba.txt", "r+t");
+	entrada = fopen("prueba.txt", "r+b");
+	char c;
+	while ((c = fgetc(entrada)) != EOF) {
+		printf("%c", c);
+	}
 	fclose(entrada);
 	return 0;
 }
