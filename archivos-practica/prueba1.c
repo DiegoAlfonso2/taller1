a709222e96ddbd64825cb6d2105f44f028d11ac2
Diego <diego.alfonso2@gmail.com>
2016-07-10 02:08:38 -0300

    Pruebas con ftell()

--- archivos-practica/prueba1.c
+++ archivos-practica/prueba1.c
@@ -6,8 +6,16 @@ int main(int argc, char const *argv[])
 	fprintf(stdout, "Abriendo archivo\n");
 	entrada = fopen("prueba.txt", "r+b");
 	char c;
+	long posiciones[300];
+	int i = 0;
+	printf("Posicion %d: %ld\n", -1, ftell(entrada));
 	while ((c = fgetc(entrada)) != EOF) {
 		printf("%c", c);
+		posiciones[i] = ftell(entrada);
+		++i;
+	}
+	for (int j = 0; j < i; ++j) {
+		printf("Posicion %d: %ld\n", j, posiciones[j]);
 	}
 	fclose(entrada);
 	return 0;
